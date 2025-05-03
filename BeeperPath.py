4bb360b5614371194e9d8fde8bd42baadaa687fe
Sayanti Chowdhury <kuasha10102@gmail.com>
2025-05-03 16:03:31 +0600

    Create BeeperPath.py

---
+++ BeeperPath.py
@@ -0,0 +1,11 @@
+from karel.stanfordkarel import *
+
+def main():
+    while beepers_present():
+        move()
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
